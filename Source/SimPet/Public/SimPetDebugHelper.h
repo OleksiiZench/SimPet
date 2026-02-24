1ead0a006400ffb6d0ab8e4b3e87143e059e7606
Oleksii Zenchenko <olekzenchenko@gmail.com>
2026-02-24 17:44:12 +0200

    feat(Core): Upgrade SimPetDebugHelper

--- Source/SimPet/Public/SimPetDebugHelper.h
+++ Source/SimPet/Public/SimPetDebugHelper.h
@@ -25,4 +25,37 @@ namespace Debug
 			UE_LOG(LogTemp, Warning, TEXT("%s"), *FinalMsg);
 		}
 	}
+	
+	static void Print(const FString &IntTitle, int32 IntValueToPrint, const FColor &Color = FColor::MakeRandomColor(), int32 InKey = -1)
+	{
+		if (GEngine)
+		{
+			const FString FinalMsg = IntTitle + TEXT(": ") + FString::FromInt(IntValueToPrint);
+
+			GEngine->AddOnScreenDebugMessage(InKey, 7.0f, Color, FinalMsg);
+
+			UE_LOG(LogTemp, Warning, TEXT("%s"), *FinalMsg);
+		}
+	}
+	
+	static void Print(const FString &BoolTitle, bool BoolValueToPrint, const FColor &Color = FColor::MakeRandomColor(), int32 InKey = -1)
+	{
+		if (GEngine)
+		{
+			FString FinalMsg = BoolTitle + TEXT(": ");
+			
+			if (BoolValueToPrint)
+			{
+				FinalMsg = BoolTitle + TEXT(": true");
+			}
+			else
+			{
+				FinalMsg = BoolTitle + TEXT(": false");
+			}
+
+			GEngine->AddOnScreenDebugMessage(InKey, 7.0f, Color, FinalMsg);
+
+			UE_LOG(LogTemp, Warning, TEXT("%s"), *FinalMsg);
+		}
+	}
 }
