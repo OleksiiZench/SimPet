83854929e529153dc3647e7a5887c17d8fa8b157
Oleksii Zenchenko <olekzenchenko@gmail.com>
2026-03-01 11:56:39 +0200

    feat(Tags): Add new tag

--- Source/SimPet/Private/SimPetGameplayTags.cpp
+++ Source/SimPet/Private/SimPetGameplayTags.cpp
@@ -20,5 +20,6 @@ namespace SimPetGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(Spawn_Point, "Spawn.Point");
 	UE_DEFINE_GAMEPLAY_TAG(Spawn_Point_HasAnimal, "Spawn.Point.HasAnimal");
 	UE_DEFINE_GAMEPLAY_TAG(Spawn_Point_ForAnimal, "Spawn.Point.ForAnimal");
+	UE_DEFINE_GAMEPLAY_TAG(Spawn_Point_HasFeed, "Spawn.Point.HasFeed");
 	UE_DEFINE_GAMEPLAY_TAG(Spawn_Point_ForFeed, "Spawn.Point.ForFeed");
 }
