906d2a9ca1be0d78f90d720175ca1cbb74d8656e
Oleksii Zenchenko <olekzenchenko@gmail.com>
2026-04-17 20:48:20 +0300

    build: Add Slate and SlateCore module dependencies

--- Source/SimPet/SimPet.Build.cs
+++ Source/SimPet/SimPet.Build.cs
@@ -22,8 +22,7 @@ public class SimPet : ModuleRules
 
         PrivateDependencyModuleNames.AddRange(new string[] { });
 
-        // Uncomment if you are using Slate UI
-        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+        PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
         // Uncomment if you are using online features
         // PrivateDependencyModuleNames.Add("OnlineSubsystem");
