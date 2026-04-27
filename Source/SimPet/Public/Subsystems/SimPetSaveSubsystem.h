21264647cc8707e4a1e890e52172fc9062610ee4
OleksiiZench <olekzenchenko@gmail.com>
2026-04-27 20:59:26 +0300

    feat(Save): Create classes that will be used to store records

---
+++ Source/SimPet/Public/Subsystems/SimPetSaveSubsystem.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2025 Oleksii Zenchenko. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "SimPetSaveSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPET_API USimPetSaveSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+};
