a1b64b18d77b7b5404b11b0f65bcb784796da35a
OleksiiZench <olekzenchenko@gmail.com>
2026-05-30 21:52:43 +0300

    feat(Save): Add the SimPetSavable interface

---
+++ Source/SimPet/Private/Interfaces/SimPetSavable.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2025 Oleksii Zenchenko. All Rights Reserved.
+
+
+#include "Interfaces/SimPetSavable.h"
+
+// Add default functionality here for any ISimPetSavable functions that are not pure virtual.
---
+++ Source/SimPet/Public/Interfaces/SimPetSavable.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2025 Oleksii Zenchenko. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+
+#include "SimPetSavable.generated.h"
+
+class USimPetSaveGame;
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class USimPetSavable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class SIMPET_API ISimPetSavable
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Save System")
+	void SaveActorData(USimPetSaveGame* SaveObject);
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "SimPet|Save System")
+	void LoadDataActor(USimPetSaveGame *SaveObject);
+};
