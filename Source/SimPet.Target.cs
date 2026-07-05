9a71b6d149c8c9b76ef7619c2c0e793d73be9d19
OleksiiZench <olekzenchenko@gmail.com>
2026-07-05 11:56:22 +0300

    build(Engine): Upgrade Unreal Engine to 5.8

--- Source/SimPet.Target.cs
+++ Source/SimPet.Target.cs
@@ -8,8 +8,11 @@ public class SimPetTarget : TargetRules
 	public SimPetTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
+		
+		bOverrideBuildEnvironment = true;
+		
 		ExtraModuleNames.Add("SimPet");
 	}
 }
--- Source/SimPetEditor.Target.cs
+++ Source/SimPetEditor.Target.cs
@@ -8,8 +8,11 @@ public class SimPetEditorTarget : TargetRules
 	public SimPetEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
+		
+		bOverrideBuildEnvironment = true;
+		
 		ExtraModuleNames.Add("SimPet");
 	}
 }
